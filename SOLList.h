6972eb83ba57a02c8e938d31d563b1e8ae4619c3
Isamar Lopez-Rodriguez <isamar.lo15425@gmail.com>
2017-02-14 07:54:45 -0800

    Update SOLList.h

--- SOLList.h
+++ SOLList.h
@@ -1,9 +1,7 @@
 //Specification/Implementation file
 //******************************************************************
 //
-//   Programador : Isamar López Rodríguez CCOM 3034 Sección 0U1
-//   Num. Est.   : 801-08-3174            Primer Semestre 2016-2017
-//   Asignación 5                         Prof. R. J. Colorado
+//   Programador : Isamar López Rodríguez 
 //   Archivo     : SOLList.h              Fecha : 14/10/16
 //
 //******************************************************************
